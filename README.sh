fe03fcd1bd3df6ff46ee5d72ba40b4c2d114ee8c
ragilpriambodo <32624502+ragilpriambodo@users.noreply.github.com>
2019-08-05 21:04:27 +0700

    Add files via upload
    Latihan dasar 1

---
+++ README.sh
@@ -0,0 +1,46 @@
+1. Menampilkan Hello World
+   Untuk menapilkan teks hello world, sebelum nya memasukan function main(), karena yang dibaca 
+   terlebih dahulu adalah main dan merupakan fungsi awal, lalu untuk menampikan teks hello world 
+   menggunakan perintah print('Hello, World').
+
+2. Variable
+   Variabel adalah lokasi penyimpanan yang berisi data data yang akan disimpan.
+
+   Dart memiliki 4 macam type, yaitu:
+	Type angka    =	int dan double
+	Type huruf    =	string
+	Type boolean  =	bool
+	Type array    =	list dan map
+   Di dart dapat mencakup keseluruhan type data dengan menuliskan 'var', contohnya  
+	var name = "voyager i" (var di sini dia dapat langsung membaca bahwa type datanya string)
+	var year = 1977 (var di sini dia dapat langsung membaca bahwa type datanya int / double)
+
+3. Control Flow Statement
+   Control flow statement adalah struktur perulangan dan percabangan yang terdapat pada dart.
+   
+   Dart memiliki 5 bentuk perulangan, yaitu:
+   	Perulangan For
+   	Perulangan While
+   	Perulangan Do/While
+    	Perulangan Foreach (For)
+   Dart memiliki 5 bentuk percabangan, yaitu:
+	Percabangan If
+    	Percabangan If/Else
+    	Percabangan If/Else If/Else
+    	percabangan Switch/Case
+    	Percabangan dengan Operator Ternary
+
+4. Function
+   Function biasa digunakan untuk melakukan satu blok kode yang berfungsi melakukan tugas yang
+   akan di eksekusi ketika dipanggil dari program bagian yang lain.
+	contohnya seperti ini:
+
+	 if (n == 0 || n == 1) return n;
+	 return fibonacci(n - 1) + fibonacci(n - 2);
+ 
+5. Comments
+   Comments digunakan untuk memberi petunjuk codingan, dan di tandai dengan '///' atau '/**'
+
+6. Import
+   Import digunakan untuk mengimport library library dart yang dibutuhkan, seperti "import
+  'dart:math';" digunakan untuk memanggil core library.
