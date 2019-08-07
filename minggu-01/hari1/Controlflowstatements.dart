903c3902d56cf138a00038b504d8dde959609e65
ragilpriambodo <32624502+ragilpriambodo@users.noreply.github.com>
2019-08-07 09:28:45 +0700

    Add files via upload

---
+++ minggu-01/hari1/Controlflowstatements.dart
@@ -0,0 +1,20 @@
+void main(){
+    int year = 2001;
+    var object;
+    var flybyObjects =['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+    if (year >= 2001) {
+        print("21st century");
+    } else if (year >= 1901) {
+        print("20th century");
+    }
+    for (var object in flybyObjects) {
+        print(object);
+    }
+for (int month = 1; month <= 12; month++) {
+  print(month);
+}
+
+while (year < 2016) {
+  year += 1;
+}
+}
