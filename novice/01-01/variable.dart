c356d5eedf5df3b6df8c8ac84b7886ffd0e2f041
ragilpriambodo <32624502+ragilpriambodo@users.noreply.github.com>
2019-08-07 21:42:21 +0700

    Add files via upload

---
+++ novice/01-01/variable.dart
@@ -0,0 +1,16 @@
+void main(){
+
+    var name = "Voyager I";
+    var year = 1977;
+    var antenaDiameter = 3.7;
+    var flybyObject = ["Jupiter", "Saturn", "Uranus", "Neptune"];
+
+    var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
+    print("Name $name. Year $year tahun. AntenaDiameter $antenaDiameter cm.");
+    print("FlyByObject: $flybyObject");
+    print("Image: $image");
+}
