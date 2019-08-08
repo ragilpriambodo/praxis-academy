dd83b46486bab5e3b5574adb013b8ed0c1c89c78
ragilpriambodo <ragilpriambodo61@gmail.com>
2019-08-08 10:37:42 +0700

    lengthlist

---
+++ novice/01-04/lengthlist.dart
@@ -0,0 +1,12 @@
+main(){
+  List<int> angka = List(5);
+  angka[0] = 10;
+  angka[1] = 20;
+  angka[2] = 30;
+  angka[3] = 40;
+  angka[4] = 50;
+
+for (int tampilangka in angka){
+  print(tampilangka);
+}
+}
