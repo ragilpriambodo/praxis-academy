6f44dab1d1f4400b62de941bd3a41c1be0baae3e
ragilpriambodo <ragilpriambodo61@gmail.com>
2019-08-08 10:29:04 +0700

    list

---
+++ novice/01-04/list.dart
@@ -0,0 +1,8 @@
+void main(){
+  List salam = ["pagi","siang","malam"];
+  print("jumlah element / panjang list : ${salam.length}");
+
+  for (int i=0; i < salam.length; i++){
+    print(salam[i]);
+  }
+}
