50edf265de6f30887a561fd24401c4acc6d56d2b
ragilpriambodo <32624502+ragilpriambodo@users.noreply.github.com>
2019-08-14 16:26:07 +0700

    launchsceen dan icon

--- novice/02-03/addingassetandimages/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'dart:async' show Future;
-import 'package:flutter/services.dart' show rootBundle;
-
-void main() => runApp(MyApp());
-Future<String> loadAsset() async{
-  return await rootBundle.loadString(key)
-}
