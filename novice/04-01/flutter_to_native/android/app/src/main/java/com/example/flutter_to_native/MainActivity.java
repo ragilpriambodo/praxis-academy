527b25c99fc16ccf5f3bb2552451acfed62cd6ac
ragilpriambodo <32624502+ragilpriambodo@users.noreply.github.com>
2019-08-27 09:56:28 +0700

    up

---
+++ novice/04-01/flutter_to_native/android/app/src/main/java/com/example/flutter_to_native/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.flutter_to_native;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "flutter.native/helper";
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+            new MethodChannel.MethodCallHandler() {
+              @Override
+              public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                if (call.method.equals("helloFromNativeCode")) {
+                  String greetings = helloFromNativeCode();
+                  result.success(greetings);
+                }
+              }});
+  }private String helloFromNativeCode() {
+    return "Hello from Native Android Code";
+  }
+}
