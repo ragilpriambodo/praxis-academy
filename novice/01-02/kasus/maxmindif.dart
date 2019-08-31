e578630f5acee3d8caa1f0eb5a0a36db44985049
ragilpriambodo <32624502+ragilpriambodo@users.noreply.github.com>
2019-08-31 11:58:52 +0700

    kasus

---
+++ novice/01-02/kasus/maxmindif.dart
@@ -0,0 +1,28 @@
+// MaxDifference(A)
+// maxDiff <- A[1] – A[0]
+// min <- A[0]
+// for i <- 1 to length[A] do
+//     If A[i] – min > maxDiff
+//         then maxDiff <- A[i] – min
+//     If A[i] < min
+//         then min <- A[i]
+// return maxDiff
+main() {
+  var A = [10, 3, 4, 2, 20];
+  print("Maximum Difference Value: ${MaxDiff(A).toString()}");
+}
+int MaxDiff(A) {
+  int min = A[0];
+  int max = A[0];
+  for (var i=0; i<A.length; i++) {
+    if (A[i]<min) {
+      min = A[i];
+    }
+  }
+  for (var i=0; i<A.length; i++) {
+    if (A[i]>max) {
+      max = A[i];
+    }
+  }
+  return max - min;
+}
